1861f2e62caa3597f999855fe60486fd5924c4dc
kolezo_pi <kolesnikovpi2005@mail.ru>
2024-12-23 18:06:06 +0300

    Added touchsreen ROS - display_driver_py

--- display_driver_py/scripts/main.py
+++ display_driver_py/scripts/main.py
@@ -1,10 +1,12 @@
 import rospy
 from display_driver_py.srv import PlayMedia, PlayMediaRequest, PlayMediaResponse
+from geometry_msgs.msg import Pose2D
 
 import os
 import cv2
 import numpy as np
 import multiprocessing
+import threading
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
@@ -12,22 +14,32 @@ from cv_bridge import CvBridge
 from ctypes import c_char
 import signal
 
+import evdev, select
+from math import sin, cos, radians
+
 class DisplayController():
 
     def __init__(self) -> None:
         rospy.init_node("display_driver_py")
 
-        self._height = rospy.get_param("~height", 1080)
-        self._width = rospy.get_param("~width", 1080)
+        self._height = rospy.get_param("~screen_resolution_h", 1080)
+        self._width = rospy.get_param("~screen_resolution_w", 1080)
         self._color_channels = rospy.get_param("~color_channels", 3)
         self._framerate = rospy.get_param("~framerate", 60)
         self._fb_name = rospy.get_param("~fb_name", "/dev/fb0")
-        self._rotate = rospy.get_param("~rotate", 0)
+        self._screen_rotate = rospy.get_param("~screen_rotate", 0)
+
+        self._use_touch = rospy.get_param("~use_touch", True)
+        touchscreen_name = rospy.get_param("~touchscreen_name", "waveshare")
+        self._touchscreen_resolution_h = rospy.get_param("~touchscreen_resolution_h", 4096)
+        self._touchscreen_resolution_w = rospy.get_param("~touchscreen_resolution_w", 4096)
+        self._touchscreen_rotate = rospy.get_param("~touch_rotate", 0)
 
         self._blank_name = rospy.get_param("~blank_name", "__BLANK__")
         
         service_PlayMedia_name = rospy.get_param("~service_PlayMedia_name", "~PlayMedia")
         topic_PlayMedia_name = rospy.get_param("~topic_PlayMedia_name", "~PlayMedia")
+        topic_touchsreen_name = rospy.get_param("~topic_touchscreen_name", "~touchscreen")
 
         self._semaphore = multiprocessing.Semaphore(1)
         self._showing_file = multiprocessing.Array(c_char, 4096)
@@ -41,17 +53,33 @@ class DisplayController():
 
         self._proccess_player = multiprocessing.Process(target=self._player, args=(), daemon=True)
         self._proccess_player.start()
-        signal.signal(signal.SIGINT, self.stop)
 
         PlayMedia_subscriber = rospy.Subscriber(topic_PlayMedia_name, Image, callback=self._img_sub)
         PlayMedia_service = rospy.Service(service_PlayMedia_name, PlayMedia, self._requester)
+        
+        if self._use_touch:
+            self._touchsreen_publisher = rospy.Publisher(topic_touchsreen_name, Pose2D, queue_size=1)
+            flag = 0
+            for device in evdev.list_devices("/dev/input/"):
+                self._touch = evdev.InputDevice(device)
+                if (touchscreen_name in str(self._touch.name).lower()):
+                    flag = 1
+                    break
+            if flag==0:
+                rospy.logerr(f"Touchscreen \"{touchscreen_name}\" not founded")
+            self._thread_touchscreen = threading.Thread(target=self._touchscreen, args=(), daemon=True)
+            self._thread_touchscreen.start()
 
+        signal.signal(signal.SIGINT, self.stop)
         rospy.loginfo("display_driver_py INITED")
         rospy.spin()
 
     def _img_sub(self, image_msg:Image):
         cv_image = self._cvBridge.imgmsg_to_cv2(image_msg, "bgr8")
-        self._fb[:]=cv2.rotate(cv_image, self._rotate)
+        if self._screen_rotate==0:
+            self._fb[:]=cv_image
+        else:
+            self._fb[:]=cv2.rotate(cv_image, self._screen_rotate-1)
         
     def _requester(self, request:PlayMediaRequest):
         response = PlayMediaResponse()
@@ -98,11 +126,14 @@ class DisplayController():
             lineType)
             bottomLeftCornerOfText = (bottomLeftCornerOfText[0], bottomLeftCornerOfText[1]+40)
 
-        self._fb[:]+=cv2.rotate(img, self._rotate)
+        if self._screen_rotate==0:
+            self._fb[:]+=img
+        else:
+            self._fb[:]+=cv2.rotate(img, self._screen_rotate-1)
 
     def _player(self) -> None:
         prev_cap = ''
-        while True:
+        while not rospy.is_shutdown():
             self._semaphore.acquire()
             current_file = self._showing_file.value.decode()
             is_played = self._is_played.value
@@ -122,7 +153,10 @@ class DisplayController():
                 if is_played == 0:
                     img = cv2.imread(current_file)
                     try:
-                        self._fb[:self._height, :self._width, :] = cv2.rotate(img, self._rotate)
+                        if self._screen_rotate==0:
+                            self._fb[:self._height, :self._width, :]=img
+                        else:
+                            self._fb[:self._height, :self._width, :]=cv2.rotate(img, self._screen_rotate-1)
                     except Exception as e:
                         self.drawError(f"Couldn`t show image.\nDetails: {e}")
 
@@ -146,7 +180,10 @@ class DisplayController():
                     self._semaphore.release()
                     cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
                 else:
-                    self._fb[:] = cv2.rotate(src, self._rotate)
+                    if self._screen_rotate==0:
+                        self._fb[:self._height, :self._width, :]=src
+                    else:
+                        self._fb[:self._height, :self._width, :]=cv2.rotate(src, self._screen_rotate-1)
                     fps.sleep()
  
             else:
@@ -155,12 +192,51 @@ class DisplayController():
                     self._semaphore.acquire()
                     self._is_played.value = 1
 
+    def _touchscreen(self) -> None:
+        flag = 0
+        x=0
+        y=0
+        msg = Pose2D()
+        self._touch.grab()
+        while not rospy.is_shutdown():
+            _,_,_ = select.select([self._touch], [], [])
+            for event in self._touch.read():
+                if event.type == evdev.ecodes.EV_ABS:
+                    if event.code == 0:
+                        # y = self._touchscreen_resolution_h-event.value
+                        x = event.value
+                    elif event.code == 1:
+                        y = event.value
+                        flag = 1
+                if flag==1:
+                    flag = 0
 
-    def stop(self, arg1=None, arg2=None):
+                    rotate = radians(90*self._touchscreen_rotate)                    
+                    x_ = x*cos(rotate)+y*sin(rotate)
+                    y_ = -x*sin(rotate)+y*cos(rotate)
+
+                    if self._touchscreen_rotate==1:
+                        y_+=self._touchscreen_resolution_w
+                    elif self._touchscreen_rotate==2:
+                        y_+=self._touchscreen_resolution_h
+                        x_+=self._touchscreen_resolution_w
+                    elif self._touchscreen_rotate==3:
+                        x_+=self._touchscreen_resolution_h
+
+                    msg.x = int(x_/self._touchscreen_resolution_w*self._width)
+                    msg.y = int(y_/self._touchscreen_resolution_h*self._height)
+                    self._touchsreen_publisher.publish(msg)
+
+    def stop(self, arg1=None, arg2=None) -> None:
         self._proccess_player.kill()
         self._proccess_player.join()
 
+        if self._use_touch:
+            self._touch.ungrab()
+
         self._fb[:]=0
+
+        rospy.loginfo("display_driver_py SHUTDOWN")
         exit(0)
 
 
