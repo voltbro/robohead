f8e11759ed9d13a6ac45bb20ab9d78d78ca8b4a7
kolezo_pi <kolesnikovpi2005@mail.ru>
2025-01-15 16:13:44 +0300

    update sensor_driver

--- sensor_driver/scripts/main.py
+++ sensor_driver/scripts/main.py
@@ -6,31 +6,27 @@ import rospy
 from sensor_msgs.msg import BatteryState
 
 class SensorDriver():
-    def __init__(self):
-        rospy.init_node('sensor_driver')
+    def callback(self, e):
+        msg = BatteryState()
+        msg.voltage = self.ina219.getBusVoltage_V()
+        msg.current = self.ina219.getCurrent_mA()/1000
+        self.pub.publish(msg)
 
+    def __init__(self):
         topic_name = rospy.get_param("~topic_name", "~bat")
         publish_rate = rospy.get_param("~publish_rate", 5)
 
         i2c_address = rospy.get_param("~i2c_address", 0x43)
         i2c_bus = rospy.get_param("~i2c_bus", 1)
 
-        ina219 = INA219(i2c_bus=i2c_bus, addr=i2c_address)
+        self.ina219 = INA219(i2c_bus=i2c_bus, addr=i2c_address)
 
-        pub = rospy.Publisher(topic_name, BatteryState, queue_size=10)
-        rate = rospy.Rate(publish_rate)
+        self.pub = rospy.Publisher(topic_name, BatteryState, queue_size=10)
+        rospy.Timer(rospy.Duration(1/publish_rate), self.callback)
 
         rospy.loginfo("sensor_driver INITED")
 
-        msg = BatteryState()
-        while not rospy.is_shutdown():
-            msg.voltage = ina219.getBusVoltage_V()
-            msg.current = ina219.getCurrent_mA()/1000
-            pub.publish(msg)
-            rate.sleep()
-
 if __name__ == '__main__':
-    try:
-        obj = SensorDriver()
-    except rospy.ROSInterruptException:
-        pass
+    rospy.init_node('sensor_driver')
+    SensorDriver()
+    rospy.spin()
