389f7007877d86c59d8f8f1d475c1c82a0c152e8
kolezo_pi <kolesnikovpi2005@mail.ru>
2025-03-10 18:18:25 +0300

    update ball_tracking_action at turtlebot_controller

--- turtlebot_controller/scripts/turtlebot_controller_actions/std_ball_tracker/action.py
+++ turtlebot_controller/scripts/turtlebot_controller_actions/std_ball_tracker/action.py
@@ -5,6 +5,7 @@ import cv2
 from cv_bridge import CvBridge
 import numpy as np
 import threading
+import math
 
 class BallTracker():
     def __init__(self, turtlebot_controller:TurtlebotController, script_path):
@@ -13,97 +14,132 @@ class BallTracker():
         self.cvBridge = CvBridge()
         self.script_path = script_path
 
-        self.resized_camera_resolution = (270,270)
-        self.original_camera_resolution = (640,480)
-        self.screen_resolution = (1080,1080)
-        self.ball_xy = (0,0)
+        self.calibration_radius = 40 # радиус, который срезается для извлечения калибровочного цвета
+        self.calibration_radius_delta = 10 # погрешность, которая отнимается от радиуса срезаемого изображения (получается срез = круг радиусом radius_cal-delta_cal), чтобы человек наверняка попал
 
-        self.thread_ball_tracker = threading.Thread(target=self.ball_tracker)
-        self.thread_neck_mover = threading.Thread(target=self.neck_mover)
+        self.tracker_zone_radius = 30
+        self.tracker_distance_radius = 40
+        self.tracker_distance_radius_delta = 5
 
-        self.neck_mover_step_duration = 0.1
-        self.neck_mover_step_value = 2
-        self.neck_mover_constraint_vertical = (-30,30)
-        self.neck_mover_constraint_horizontal = (-30,30)
-
-        self.delta_k = 3/((self.resized_camera_resolution[0]//2)**2+(self.resized_camera_resolution[1]//2)**2)**0.5
-        # показывает, во сколько раз надо увеличить neck_mover_step_value, если шарик находится в самом краю изображения
-
-        self.threshold_zone = 15
         self.hsv_filter = ((16,40),(180,255),(120,255)) # (hlow,hhigh), (slow,shigh), (vlow,vhigh)
         # hsv_filter изменяется при калибровке
 
-        self.radius_cal = 30 # радиус, который срезается для извлечения калибровочного цвета
-        self.delta_cal = 10 # погрешность, которая прибавляется к радиусу срезаемого изображения (круг радиусом radius_cal), чтобы человек наверняка попал
+        self.resized_camera_resolution = (270,270)
+        self.original_camera_resolution = (640,480)
+        self.screen_resolution = (1080,1080)
+        self.ball_xyr = (int(self.resized_camera_resolution[0]//2), int(self.resized_camera_resolution[1]//2), self.tracker_distance_radius)
+
+        self.thread_ball_tracker = threading.Thread(target=self.ball_tracker)
+        self.thread_mover = threading.Thread(target=self.mover)
+
+        self.mover_neck_step_duration = 0.1
+        self.mover_neck_step_value = 2
+        self.mover_constraint_vertical = (-30,30)
+        self.mover_constraint_horizontal = (-30,30)
+
+        self.delta_k = 3/((self.resized_camera_resolution[0]//2)**2+(self.resized_camera_resolution[1]//2)**2)**0.5
+        # показывает, во сколько раз надо увеличить mover_step_value, если шарик находится в самом краю изображения
 
     def start(self, duration_tracking, duration_calibrate):
-        self.hsv_filter = self.calibrate(duration_calibrate, self.radius_cal, self.delta_cal)
+        self.hsv_filter = self.calibrate(duration_calibrate)
         self.thread_ball_tracker.start()
-        self.thread_neck_mover.start()
+        self.thread_mover.start()
         rospy.Timer(rospy.Duration(duration_tracking), callback=self.finish, oneshot=True)
 
     def join(self):
         self.thread_ball_tracker.join()
-        self.thread_neck_mover.join()
+        self.thread_mover.join()
 
     def finish(self, e):
         self.is_run = False
-
-    def neck_mover(self):
-        msg = NeckSetAngleRequest()
-        msg.is_blocking = 1
-        msg.duration = self.neck_mover_step_duration
-        msg.vertical_angle = 0
-        msg.horizontal_angle = 0
-
-        msg_twist = Twist()
-
-        cur_horizontal_angle = 0
+    def sign(self, x:int):
+        if x==0: return 0
+        elif x<0: return -1
+        elif x>0: return 1
+    def mover(self):
+        msg_neck = NeckSetAngleRequest()
+        msg_neck.is_blocking = 1
+        msg_neck.duration = self.mover_neck_step_duration
+        msg_neck.vertical_angle = 0
+        
+        mover_neck_step_value = self.mover_neck_step_value
         cur_vertical_angle = 0
 
-        step = self.neck_mover_step_value
-        threshold_zone = self.threshold_zone
+        msg_cmd_vel = Twist()
 
-        center_x = int(self.resized_camera_resolution[0]/2)
-        center_y = int(self.resized_camera_resolution[1]/2)
-        prev_ball_xy = self.ball_xy
+        resized_camera_resolution = self.resized_camera_resolution
+        center_x = int(resized_camera_resolution[0]/2)
+        center_y = int(resized_camera_resolution[1]/2)
+
+        tracker_distance_radius = self.tracker_distance_radius
+        tracker_distance_radius_delta = self.tracker_distance_radius_delta
+        tracker_zone_radius = self.tracker_zone_radius
+        prev_ball_xyr = self.ball_xyr
+
+        timer_start = rospy.get_time()
+
         while self.is_run and not rospy.is_shutdown():
-            x, y = self.ball_xy
-            if (prev_ball_xy[0]==x) and (prev_ball_xy[1]==y):
+            x, y, r = self.ball_xyr
+
+            if (rospy.get_time() - timer_start) > 2:
+                msg_cmd_vel = Twist()
+                self.turtlebot_controller.turtlebot_pub_cmd_vel.publish(msg_cmd_vel)
+                timer_start = rospy.get_time()
+
+            if (prev_ball_xyr[0]==x) and (prev_ball_xyr[1]==y) and (prev_ball_xyr[2]==r):
                 continue
-            delta = (((center_x-x)**2+(center_y-y)**2)**0.5)*self.delta_k
-            delta = min(max(delta, 1),3)
-            step_d = int(step*delta)
+            timer_start = rospy.get_time()
 
-            if x<(center_x-threshold_zone):
-                msg_twist.angular.z = -0.2*delta
-                # if (cur_horizontal_angle+step_d)<=self.neck_mover_constraint_horizontal[1]:
-                #     cur_horizontal_angle+=step_d
-                #     msg.horizontal_angle = cur_horizontal_angle
-            elif x>(center_x+threshold_zone):
-                msg_twist.angular.z = 0.2*delta
-                # if (cur_horizontal_angle-step_d)>=self.neck_mover_constraint_horizontal[0]:
-                #     cur_horizontal_angle-=step_d
-                #     msg.horizontal_angle = cur_horizontal_angle
+            prev_ball_xyr = (x,y,r)
+
+            delta_r = tracker_distance_radius - r
+            delta_x = x - center_x
+            delta_y = center_y - y
+
+            if abs(delta_r) > tracker_distance_radius_delta:
+                velocity = min(max(0.1*abs(delta_r)/(resized_camera_resolution[0]/4), 0.05), 0.2)
+                msg_cmd_vel.linear.x = velocity*self.sign(delta_r)
             else:
-                msg_twist.angular.z = 0
+                msg_cmd_vel.linear.x = 0
 
-            if y<(center_y-threshold_zone):
-                if (cur_vertical_angle+step_d)<=self.neck_mover_constraint_vertical[1]:
-                    cur_vertical_angle+=step_d
-                    msg.vertical_angle = cur_vertical_angle
-            elif y>(center_y+threshold_zone):
-                if (cur_vertical_angle-step_d)>=self.neck_mover_constraint_vertical[0]:
-                    cur_vertical_angle-=step_d
-                    msg.vertical_angle = cur_vertical_angle
+            if (delta_x**2+delta_y**2) > (tracker_zone_radius)**2:
+                if abs(cur_vertical_angle+mover_neck_step_value*self.sign(delta_y))<=30:
+                    cur_vertical_angle+=mover_neck_step_value*self.sign(delta_y)
+                    msg_neck.vertical_angle = int(cur_vertical_angle)
 
-            self.turtlebot_controller.neck_driver_srv_NeckSetAngle(msg)
-            self.turtlebot_controller.turtlebot_pub_cmd_vel.publish(msg_twist)
-        msg_twist.angular.z = 0
-        self.turtlebot_controller.turtlebot_pub_cmd_vel.publish(msg_twist)
+                velocity = min(max(0.2*abs(delta_x)/(resized_camera_resolution[0]/4), 0.1), 0.4)
+                msg_cmd_vel.angular.z = velocity*self.sign(delta_x)
+            else:
+                msg_cmd_vel.angular.z = 0
+
+            self.turtlebot_controller.neck_driver_srv_NeckSetAngle(msg_neck)
+            self.turtlebot_controller.turtlebot_pub_cmd_vel.publish(msg_cmd_vel)
+
+        msg_cmd_vel = Twist()
+        self.turtlebot_controller.turtlebot_pub_cmd_vel.publish(msg_cmd_vel)
 
     def ball_tracker(self):
         prev_img = self.turtlebot_controller.usb_cam_image_raw
+        resized_camera_resolution = self.resized_camera_resolution
+        original_camera_resolution = self.original_camera_resolution
+        screen_resolution = self.screen_resolution
+        # центр resized изображения
+        center_x_resized = resized_camera_resolution[0]//2
+        center_y_resized = resized_camera_resolution[1]//2
+
+        tracker_zone_radius = self.tracker_zone_radius
+        tracker_distance_radius = self.tracker_distance_radius
+        tracker_distance_radius_delta = self.tracker_distance_radius_delta
+
+        hlow = self.hsv_filter[0][0]
+        slow = self.hsv_filter[1][0]
+        vlow = self.hsv_filter[2][0]
+        hhigh = self.hsv_filter[0][1]
+        shigh = self.hsv_filter[1][1]
+        vhigh = self.hsv_filter[2][1]
+        HSVLOW  = np.array([hlow, slow, vlow])
+        HSVHIGH = np.array([hhigh, shigh, vhigh])
+        rect_cut = min(original_camera_resolution)
 
         while self.is_run and not rospy.is_shutdown():
             if (prev_img!=self.turtlebot_controller.usb_cam_image_raw):
@@ -111,9 +147,8 @@ class BallTracker():
 
                 cv_image = self.cvBridge.imgmsg_to_cv2(prev_img, "bgr8")
                 # cv2.imwrite(self.script_path+'1_orig.png', cv_image)
-
-                rect = min(self.original_camera_resolution)
-                cv_image = cv2.resize(cv_image[:rect,:rect], self.resized_camera_resolution)
+                
+                cv_image = cv2.resize(cv_image[:rect_cut,:rect_cut], resized_camera_resolution)
                 cv_image = cv2.flip(cv_image, 1)
                 # cv2.imwrite(self.script_path+'2_resized.png', cv_image)
 
@@ -121,16 +156,8 @@ class BallTracker():
                 # cv2.imwrite(self.script_path+'3_blure.png', blured_image)
 
                 hsv = cv2.cvtColor(blured_image, cv2.COLOR_BGR2HSV)
-                # cv2.imwrite(self.script_path+'4_hsv.png', hsv)
+                # cv2.imwrite(self.script_path+'4_hsv_format.png', hsv)
 
-                hlow = self.hsv_filter[0][0]
-                slow = self.hsv_filter[1][0]
-                vlow = self.hsv_filter[2][0]
-                hhigh = self.hsv_filter[0][1]
-                shigh = self.hsv_filter[1][1]
-                vhigh = self.hsv_filter[2][1]
-                HSVLOW  = np.array([hlow, slow, vlow])
-                HSVHIGH = np.array([hhigh, shigh, vhigh])
                 mask = cv2.inRange(hsv,HSVLOW, HSVHIGH)
                 # cv2.imwrite(self.script_path+'5_hsv_filtered.png', mask)
 
@@ -142,23 +169,26 @@ class BallTracker():
                     c = max(cnts, key=cv2.contourArea)
                     (x, y), radius = cv2.minEnclosingCircle(c)
 
-                    if radius>10:
+                    if (radius>10) and (radius<100):
                         xy = (int(x), int(y))
-                        self.ball_xy = xy
+                        self.ball_xyr = (int(x), int(y), int(radius))
+
                         cv2.circle(cv_image, xy, int(radius), (0, 255, 0), 2)
                         cv2.circle(cv_image, xy, 1, (0, 0, 255), 3)
-                lu_angle = (self.resized_camera_resolution[0]//2-self.threshold_zone, self.resized_camera_resolution[1]//2-self.threshold_zone)
-                rd_angle = (self.resized_camera_resolution[0]//2+self.threshold_zone, self.resized_camera_resolution[1]//2+self.threshold_zone)
-                cv2.rectangle(cv_image, lu_angle, rd_angle, (0,0,255), 1)
-                cv_image = cv2.resize(cv_image, self.screen_resolution)
+                        cv2.circle(cv_image, xy, int(tracker_distance_radius+tracker_distance_radius_delta), (255, 0, 0), 1)
+                        cv2.circle(cv_image, xy, int(tracker_distance_radius-tracker_distance_radius_delta), (255, 0, 0), 1)
+
+                cv2.circle(cv_image, (center_x_resized,center_y_resized), int(tracker_zone_radius), (0, 0, 255), 1)
+                cv_image = cv2.resize(cv_image, screen_resolution)
                 self.turtlebot_controller.display_driver_pub_PlayMedia.publish(self.cvBridge.cv2_to_imgmsg(cv_image, encoding="bgr8"))
 
-    def calibrate(self, duration_calibrate, radius_cal, delta_cal):
+    def calibrate(self, duration_calibrate):
         prev_img = self.turtlebot_controller.usb_cam_image_raw
 
+        resized_camera_resolution = self.resized_camera_resolution
         # центр resized изображения
-        center_x_resized = self.resized_camera_resolution[0]//2
-        center_y_resized = self.resized_camera_resolution[1]//2
+        center_x_resized = resized_camera_resolution[0]//2
+        center_y_resized = resized_camera_resolution[1]//2
 
         # параметры для текста (обратный таймер с циферками)
         font = cv2.FONT_HERSHEY_SIMPLEX
@@ -166,6 +196,11 @@ class BallTracker():
         fontColor = (255,0,0)
         thickness = 20
         lineType = 5
+
+        calibration_radius = self.calibration_radius
+        calibration_radius_delta = self.calibration_radius_delta
+        
+        rect_cut = min(self.original_camera_resolution)
 
         timer_start = rospy.get_time()
         while ((rospy.get_time()-timer_start)<duration_calibrate) and not rospy.is_shutdown():
@@ -175,32 +210,36 @@ class BallTracker():
                 str_num = str(duration_calibrate-int(rospy.get_time()-timer_start))
                 cv_image = self.cvBridge.imgmsg_to_cv2(prev_img, "bgr8")
 
-                rect = min(self.original_camera_resolution)
-                cv_image = cv2.resize(cv_image[:rect,:rect], self.resized_camera_resolution)
-                cv_image = cv2.flip(cv_image, 1)
-                rect = cv_image.copy()
                 
-                cv2.circle(cv_image, (center_x_resized, center_y_resized), radius_cal+delta_cal, (0, 255, 0), 2)
-                cv2.circle(cv_image, (center_x_resized, center_y_resized), radius_cal, (255, 0, 0), 1)
+                cv_image = cv2.resize(cv_image[:rect_cut,:rect_cut], resized_camera_resolution)
+                cv_image = cv2.flip(cv_image, 1)
+                
+                cv2.circle(cv_image, (center_x_resized, center_y_resized), calibration_radius, (0, 255, 0), 2)
+                cv2.circle(cv_image, (center_x_resized, center_y_resized), calibration_radius-calibration_radius_delta, (255, 0, 0), 1)
 
                 screen_img = cv2.resize(cv_image, self.screen_resolution)
-                bottomLeftCornerOfText = (self.screen_resolution[0]//2-cv2.getTextSize(str_num, font, fontScale, thickness)[0][0]//2, self.screen_resolution[1]//2-2*cv2.getTextSize(str_num, font, fontScale, thickness)[0][1]//2-radius_cal)
+                bottomLeftCornerOfText = (self.screen_resolution[0]//2-cv2.getTextSize(str_num, font, fontScale, thickness)[0][0]//2, self.screen_resolution[1]//2-2*cv2.getTextSize(str_num, font, fontScale, thickness)[0][1]//2-calibration_radius)
                 cv2.putText(screen_img, str_num, tuple(bottomLeftCornerOfText), 
                 font, fontScale, fontColor, thickness, lineType)
                 self.turtlebot_controller.display_driver_pub_PlayMedia.publish(self.cvBridge.cv2_to_imgmsg(screen_img, encoding="bgr8"))
         
-        rect = rect[int(center_x_resized-radius_cal):int(center_x_resized+radius_cal), int(center_y_resized-radius_cal):int(center_y_resized+radius_cal)]
+        cv_image = self.cvBridge.imgmsg_to_cv2(prev_img, "bgr8")
+        cv_image = cv2.resize(cv_image[:rect_cut,:rect_cut], resized_camera_resolution)
+        cv_image = cv2.flip(cv_image, 1)
+
+        rect = cv_image[int(center_x_resized-calibration_radius+calibration_radius_delta):int(center_x_resized+calibration_radius-calibration_radius_delta), int(center_y_resized-calibration_radius+calibration_radius_delta):int(center_y_resized+calibration_radius-calibration_radius_delta)]
         mask = np.zeros(rect.shape[:2], dtype="uint8")
-        cv2.circle(mask, (int(rect.shape[0]//2), int(rect.shape[1]//2)), radius_cal, 255, -1)
+        cv2.circle(mask, (int(rect.shape[0]//2), int(rect.shape[1]//2)), calibration_radius-calibration_radius_delta, 255, -1)
         masked = cv2.bitwise_and(rect, rect, mask=mask)
 
         hsv = self.get_hsv_range(masked)
-        # print("hsv_filter:", hsv)
+        print("hsv_filter:", hsv)
         return hsv
     
     def get_hsv_range(self, img):
         # пробегаем по изображеняи img и выделяем диапазон цветов на нем
 
+        # cv2.imwrite(self.script_path+'get_hsv_range_img.png', img)
         blured_image = cv2.GaussianBlur(img, (15, 15), 0)
         hsv_image = cv2.cvtColor(blured_image, cv2.COLOR_BGR2HSV)
         rows, cols, _ = hsv_image.shape
@@ -208,7 +247,7 @@ class BallTracker():
         hsv_filter = [[180,0],[255,0],[255,0]] # (hlow,hhigh), (slow,shigh), (vlow,vhigh)
         for row in range(rows):
             for col in range(cols):
-                if (hsv_image[row][col][1]<=100) or (hsv_image[row][col][2]<=100):
+                if (hsv_image[row][col][1]<=70) or (hsv_image[row][col][2]<=70):
                     continue
                 hsv_filter[0][0] = min(hsv_image[row][col][0], hsv_filter[0][0])
                 hsv_filter[0][1] = max(hsv_image[row][col][0], hsv_filter[0][1])
