579bf921b58bdeaaa15006535c46a0a8a5c2039a
kolezo_pi <kolesnikovpi2005@mail.ru>
2025-03-12 19:07:17 +0300

    update setup_os

--- setupOS/start.sh
+++ setupOS/start.sh
@@ -7,17 +7,15 @@ export PYTHONPATH=$PYTHONPATH:$ROS_ROOT/core/roslib/src
 export PYTHONPATH=$PYTHONPATH:/usr/bin/python3
 export PYTHONPATH=$PYTHONPATH:/home/pi/.local/lib/python3.8/site-packages
 
-pactl set-default-sink 'alsa_output.usb-SEEED_ReSpeaker_4_Mic_Array__UAC1.0_-00.analog-stereo'
-pulseaudio -k
-pulseaudio --start -D
+# for work with another robot
+# ip of another robot:
+# export ROS_MASTER_URI=http://10.42.0.1:11311 
+# ip of robohead:
+# export ROS_HOSTNAME=10.42.0.154
 
-# for work with MORS
-#export ROS_MASTER_URI=http://10.42.0.1:11311
-#export ROS_HOSTNAME=10.42.0.154
-
-# for local work (without MORS)
+# for local work (without a robot)
 export ROS_MASTER_URI=http://127.0.0.1:11311
 export ROS_HOSTNAME=127.0.0.1
 
-roslaunch head_controller head_controller.launch useMors:=0
+roslaunch robohead_controller robohead_controller_py.launch
 
