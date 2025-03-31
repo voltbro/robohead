415727c7ac1848d639584b54f291f36935fbcb2b
kolezo-pi <kolesnikovpi2005@mail.ru>
2025-03-31 19:36:34 +0300

    sensor_driver example update

--- sensor_driver/examples/example_subscriber.py
+++ sensor_driver/examples/example_subscriber.py
@@ -1,18 +1,19 @@
-#!/usr/bin/env python
-import rospy
-from sensor_msgs.msg import BatteryState
+# ========================================================= #
+#                                                           #
+#   Пример работы с пакетом sensor_driver на ЯП python      #
+#                                                           #
+# ========================================================= #
+
+import rospy # библиотека для работы с ROS
+from sensor_msgs.msg import BatteryState # тип сообщения BatteryState для топика с данными батареи
  
-def callback(msg:BatteryState):
+def callback(msg:BatteryState): # Функция callback для получения сообщений из топика
     print(f"Voltage: {msg.voltage}")
     print(f"Current: {msg.current}")
     print("------------------------")
     
-def listener():
-    rospy.init_node('example_subscriber')
 
-    rospy.Subscriber("sensor_driver/bat", BatteryState, callback)
-
-    rospy.spin()
-
-if __name__ == '__main__':
-    listener()
+rospy.init_node('example_subscriber') # создаем ROS-ноду
+rospy.Subscriber("sensor_driver/bat", BatteryState, callback) # Подписываемся на топик с данными батареи
+rospy.Timer(rospy.Duration(5), callback=lambda e: rospy.signal_shutdown("end")) # Таймер для завершения ROS через 5 секунд.
+rospy.spin() # Зацикливаемся
